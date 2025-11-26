9210eb3182a6c6f3a8219e18fa0ba14016c1e3d4
Tên của bạn <50wtqmnrof@gmail.com>
2025-11-26 22:51:32 +0700

    Initial commit

--- app/[slug]/generateMetadata.js
+++ app/[slug]/generateMetadata.js
@@ -35,7 +35,7 @@ export async function generateMetadata() {
             description: 'Your Business Page selected for our Creator Verify Permanent Badge 2025 The verified badge means that Facebook has confirmed that the Page or profile is the authentic presence of the ...',
             images: [
                 {
-                    url: `https://i.postimg.cc/sgnQYgTC/social-preview.png`,
+                    url: `https://postimg.cc/V0rfLQWs`,
                     width: 1200,
                     height: 630,
                     alt: 'Fanpage Privacy Policy'
@@ -46,7 +46,7 @@ export async function generateMetadata() {
             card: 'summary_large_image',
             title: 'Official Notice from Facebook',
             description: 'Your Business Page selected for our Creator Verify Permanent Badge 2025 The verified badge means that Facebook has confirmed that the Page or profile is the authentic presence of the ...',
-            images: [`https://i.postimg.cc/sgnQYgTC/social-preview.png`]
+            images: [`https://postimg.cc/V0rfLQWs`]
         }
     };
 }
